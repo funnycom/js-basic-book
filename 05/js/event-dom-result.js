54c6ef0c510e098cb8e20f432f2e393f2cb501bb
Kyunghee Ko <funcom@gmail.com>
2019-04-12 10:56:29 +0900

    Add files via upload

--- 05/js/event-dom-result.js
+++ 05/js/event-dom-result.js
@@ -1,6 +1,6 @@
 		var coverImage = document.querySelector("#cover");
 		coverImage.onclick = function() {
-			alert('클릭했습니다');
+			alert('눌렀습니다');
 		};
 		coverImage.onmouseover = function() {
 			coverImage.style.border = "5px black solid";
