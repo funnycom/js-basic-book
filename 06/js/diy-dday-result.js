aea27ccb231015b26f775eee137f70b0fcb9c346
Kyunghee Ko <funcom@gmail.com>
2019-08-05 13:48:05 +0900

    Update diy-dday-result.js

--- 06/js/diy-dday-result.js
+++ 06/js/diy-dday-result.js
@@ -15,7 +15,7 @@ function calcDate(days) {
 	var future = toFirst + days*(1000*60*60*24);  // 처음 만난 날에 밀리초로 바꾼 100일을 더합니다.
 	var someday = new Date(future);  // future 값을 사용해 Date 객체의 인스턴스를 만듭니다.
 	var year = someday.getFullYear( );  // ‘연도’를 가져와 year 변수에 저장합니다.
-	var month = someday.getMonth( );  // ‘월’을 가져와 month 변수에 저장합니다.
+	var month = someday.getMonth( ) + 1;  // ‘월’을 가져와 month 변수에 저장합니다.
 	var date = someday.getDate( );  // ‘일’을 가져와 date 변수에 저장합니다.
 	document.querySelector("#date"+days).innerText = year + "년 " + month + "월 " + date + "일";
 }
