75631eff38ea4788706af58347c9cf1590c20f5c
Joon <jaejoon0721@ajou.ac.kr>
2023-01-11 20:56:30 +0900

    diy-event실습 중  onclick에서는 open이랑 close라는 이름의 함수는 작동 안함

--- 05/js/diy-event-result.js
+++ 05/js/diy-event-result.js
@@ -1,9 +1,12 @@
-		function showDetail() {
-			document.querySelector('#desc').style.display = "block";	
-			document.querySelector('#open').style.display = "none";  
-		}
+const o = document.querySelector("#open");
+const c = document.querySelector("#desc");
 
-		function hideDetail() {
-			document.querySelector('#desc').style.display = "none";	   
-			document.querySelector('#open').style.display = "block";	 
-		}
+let showDetail = () => {
+  o.style.display = "none";
+  c.style.display = "block";
+};
+
+function hideDetail() {
+  document.querySelector("#desc").style.display = "none";
+  document.querySelector("#open").style.display = "block";
+}
--- 05/js/diy-event.js
+++ 05/js/diy-event.js
@@ -1,9 +1,22 @@
-		function showDetail() {  // '상세 설명 보기'를 클릭했을 때 상세 설명을 보여주는 함수
-			document.querySelector('#desc').style.display = "block";	// 상세 설명 부분을 화면에 표시
-			document.querySelector('#open').style.display = "none";   // '상세 설명 보기' 단추를 화면에서 감춤
-		}
-		
-		function hideDetail() {  // '상세 설명 닫기'를 클릭했을 때 상세 설명을 감추는 함수
-			document.querySelector('#desc').style.display = "none";	   // 상세 설명 부분을 화면에서 감춤
-			document.querySelector('#open').style.display = "block";	 // '상세 설명 보기' 단추를 화면에 표시
-		}
+const o = document.querySelector("#open");
+const c = document.querySelector("#desc");
+
+let shows = () => {
+  o.style.display = "none";
+  c.style.display = "block";
+};
+
+let hides = () => {
+  o.style.display = "block";
+  c.style.display = "none";
+};
+
+let open = () => {
+  o.style.display = "none";
+  c.style.display = "block";
+};
+
+let close = () => {
+  o.style.display = "block";
+  c.style.display = "none";
+};
