aa96e33263a9fa4d496e7f329e5447f9c8bcc011
Joon <jaejoon0721@ajou.ac.kr>
2023-01-13 17:40:55 +0900

    퀴즈 풀이 2분컷!

--- 07/js/quiz-1.js
+++ 07/js/quiz-1.js
@@ -1,11 +1,14 @@
-		var numbers = [2, 4, 6, 8, 10];
-		showArray(numbers);
+var numbers = [2, 4, 6, 8, 10];
+showArray(numbers);
 
-		function showArray(arr) {
-			var str = "<table><tr>";
-			for (var i=0; i<arr.length; i++) {
-				str += "<td>" + arr[i] + "</td>";
-			}
-			str += "</tr></table>";
-			document.write(str);
-		}
+function showArray(arr) {
+  var str = "<table><tr>";
+  let sum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+    str += "<td>" + arr[i] + "</td>";
+  }
+
+  str += `<td>${sum}</td></tr></table>`;
+  document.write(str);
+}
