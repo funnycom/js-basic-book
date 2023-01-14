a52bbc095f9a60d8c59634871bee5c72f5bbb765
junly21 <jaejoon0721@ajou.ac.kr>
2023-01-15 00:01:09 +0900

    if 문 활용 display 표시/사라지게

--- 08/js/product2.js
+++ 08/js/product2.js
@@ -1,9 +1,18 @@
+let isOpen = false;
 let view = document.getElementById("view");
 let detail = document.getElementById("detail");
 
 view.addEventListener("click", showDetail);
 
 function showDetail() {
-  detail.style.display = "block";
+  if (isOpen == false) {
+    detail.style.display = "block";
+    view.innerHTML = "상세 설명 닫기";
+    isOpen = true;
+  } else {
+    detail.style.display = "none";
+    view.innerHTML = "상세 설명 보기";
+    isOpen = false;
+  }
 }
 //contribution test22
