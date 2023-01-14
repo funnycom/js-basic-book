2695692029ed0335a09d78e2a3b5972dba4f2ebd
Joon <jaejoon0721@ajou.ac.kr>
2023-01-14 23:06:32 +0900

    8-4까지 실습

--- 08/js/domevent.js
+++ 08/js/domevent.js
@@ -1,8 +1,9 @@
-var pic = document.querySelector('#pic');
-    
-function changePic() {			
+var pic = document.querySelector("#pic");
+pic.addEventListener("mouseover", changePic);
+pic.addEventListener("mouseout", originPic);
+function changePic() {
   pic.src = "images/boy.png";
 }
- function originPic() {
+function originPic() {
   pic.src = "images/girl.png";
 }
