92adcb56cf540dfa2bc5f04bf944627c45abedc1
Joon <jaejoon0721@ajou.ac.kr>
2023-01-14 23:45:39 +0900

    js로 display click event시 생기게 하기

---
+++ 08/js/product2.js
@@ -0,0 +1,8 @@
+let view = document.getElementById("view");
+let detail = document.getElementById("detail");
+
+view.addEventListener("click", showDetail);
+
+function showDetail() {
+  detail.style.display = "block";
+}
